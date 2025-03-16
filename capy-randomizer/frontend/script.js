6c8191540f0656ae18324c027d6a97e7fc99d260
Nicolas Keyt <142526482+NicolasKEYT@users.noreply.github.com>
2025-03-16 18:05:58 -0300

    Delete capy-randomizer directory

--- capy-randomizer/frontend/script.js
+++
@@ -1,8 +0,0 @@
-async function getCapy() {
-    const response = await fetch("http://backend:25000/capy");
-    const data = await response.json();
-    document.getElementById("capyImage").src = data.capybara;
-}
-
-// Carregar uma capivara aleatória ao abrir a página
-getCapy();
