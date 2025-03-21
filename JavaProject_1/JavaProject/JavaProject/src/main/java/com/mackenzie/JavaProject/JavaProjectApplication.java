6618d96268352baadd9e3a8b392a3d73b87c7c61
Diego Estevão Lopes de Queiroz <deadzone8727@gmail.com>
2025-03-21 11:19:47 +0000

    Adiciona controlador REST e configuração de porta do servidor; atualiza texto de imagem no HTML

--- JavaProject_1/JavaProject/JavaProject/src/main/java/com/mackenzie/JavaProject/JavaProjectApplication.java
+++ JavaProject_1/JavaProject/JavaProject/src/main/java/com/mackenzie/JavaProject/JavaProjectApplication.java
@@ -2,10 +2,20 @@ package com.mackenzie.JavaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class JavaProjectApplication {
-
+	@RestController
+	public class HelloController {
+	
+		@GetMapping("/")
+		public String hello() {
+			return "API funcionando!";
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProjectApplication.class, args);
 	}
